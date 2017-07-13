87c4513a1f0e2fe41ee217f9214a7ca9aee61db6
John Staveley <john@staveley.org>
2017-07-13 16:51:15 +0100

    Added Rabbit Sender project

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	        ConnectionFactory factory = new ConnectionFactory();
+	        // "guest"/"guest" by default, limited to localhost connections
+	        factory.UserName = "user";
+	        factory.Password = "LeedsSharp4";
+	        factory.VirtualHost = "vhost";
+	        factory.HostName = "192.168.99.100";
+
+	        IConnection conn = factory.CreateConnection();
+		}
+    }
+}
