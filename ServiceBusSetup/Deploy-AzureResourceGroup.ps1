7589d6a3e4d9899b15b1b3e6e44d6d6e2828b75c
John Staveley <john@staveley.org>
2017-07-13 09:09:26 +0100

    Put in ARM Template to create environment

---
+++ ServiceBusSetup/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,96 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+    Deploys an Azure Resource ReadWriter template
+
+#>
+
+param(
+ 
+ [string]
+ $subscriptionId = "a0d704f5-f3ad-49cb-ad02-bda68d45ed1f",
+
+ [string]
+ $resourceGroupLocation = "uksouth",
+
+ [string]
+ $environmentName = "LeedsSharp2"
+)
+
+<#
+.SYNOPSIS
+    Registers RPs
+#>
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+function Check-Session () {
+    $Error.Clear()
+    #if context already exist
+    Get-AzureRmContext -ErrorAction Continue
+    $Error
+    foreach ($eachError in $Error) {
+        if ($eachError.Exception.ToString() -like "*Run Login-AzureRmAccount to login.*") {
+            Login-AzureRmAccount
+        }
+    }
+    $Error.Clear();
+}
+
+# Setup variables
+$workingDirectory = "C:\Work\MessagingPresentation\ServiceBusSetup\"
+$parametersFile = "azuredeploy.parameters.json"
+$templateFile = "azuredeploy.json"
+$templateFilePath = $workingDirectory + $templateFile
+$parametersFilePath = $workingDirectory + $parametersFile
+$resourceGroupName = $environmentName
+$ErrorActionPreference = "Stop"
+
+# sign in
+Write-Host "Logging in...";
+Check-Session
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+
+# Register RPs
+$resourceProviders = @("microsoft.insights","microsoft.sql","microsoft.storage","microsoft.web","microsoft.servicebus");
+if($resourceProviders.length) {
+    Write-Host "Registering resource providers"
+    foreach($resourceProvider in $resourceProviders) {
+        RegisterRP($resourceProvider);
+    }
+}
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    Start-Sleep(5) # Give it some time to create the resource group
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+if(Test-Path $parametersFilePath) {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+} else {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+}
