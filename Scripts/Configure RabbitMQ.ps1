dfaf958b3e28e8bb50483d67cbe8e3cca8b2377f
John Staveley <john@staveley.org>
2017-07-13 16:42:51 +0100

    Minor mods to rabbit mq script

--- Scripts/Configure RabbitMQ.ps1
+++ Scripts/Configure RabbitMQ.ps1
@@ -2,30 +2,32 @@ $ipAddressOfRabbitMQ = "192.168.99.100"
 $userName = "user"
 $password = "LeedsSharp4"
 
-# You will need to git clone them from https://github.com/mariuszwojcik/RabbitMQTools and then change to the install directory
-cd "C:/Program Files (x86)/RabbitMQTools" 
+cd "C:/Program Files (x86)/RabbitMQTools"
 $rabbitMQToolsModule = Get-Module | Where-Object { $_.Name -eq "RabbitMQTools" }
 if ($rabbitMQToolsModule -eq $null) {
     Import-Module .\RabbitMQTools
     }
 $exchangeName = "Products"
-$queueName = "Products"
+$queueName = "LargeProducts"
 $queueRoutingKey = "Large"
 
 # Create exchange if not already present
-$exchange = Get-RabbitMQExchange -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password | Where-Object { $_.Name -eq $exchangeName}
+$exchange = Get-RabbitMQExchange -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password `
+    | Where-Object { $_.Name -eq $exchangeName}
 if ($exchange -eq $null) {
     Add-RabbitMQExchange -Name $exchangeName -Type "topic" -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password -VirtualHost vhost
 }
 
 # Create Queue if not already present
-$queue = Get-RabbitMQQueue -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password | Where-Object { $_.Name -eq $queueName }
+$queue = Get-RabbitMQQueue -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password `
+    | Where-Object { $_.Name -eq $queueName }
 if ($queue -eq $null) {
     Add-RabbitMQQueue -Name $queueName -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password  -VirtualHost vhost
 }
 
 # Create queue binding if not already present
-$productQueueBinding = Get-RabbitMQQueueBinding -Name $queueName -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password | Where-Object { $_.destination -eq $exchangeName -and $_.routing_key -eq $queueRoutingKey}
+$productQueueBinding = Get-RabbitMQQueueBinding -Name $queueName -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password `
+    | Where-Object { $_.destination -eq $exchangeName -and $_.routing_key -eq $queueRoutingKey}
 if ($productQueueBinding -eq $null) {
     Add-RabbitMQQueueBinding -Name $queueName -ExchangeName $exchangeName -RoutingKey "$queueRoutingKey" -ComputerName $ipAddressOfRabbitMQ -UserName $userName -Password $password -VirtualHost vhost 
 }    
